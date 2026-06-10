26d792b98106a8cd41a06966f9ed80e406d4d7dd
Gustavo-Vinicius <gustavovinicimos12345@gmail.com>
2026-06-10 20:06:57 -0300

    feat(auth0): adicionando rotinas de login para aplicação.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,15 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/medicos")
@@ -40,6 +38,8 @@ public class MedicoController {
     @GetMapping("/{id}")
     public ResponseEntity detalhar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
+
+
 
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
